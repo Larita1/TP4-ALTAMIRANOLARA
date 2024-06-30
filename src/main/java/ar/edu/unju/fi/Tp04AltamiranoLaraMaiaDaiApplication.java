f979770e865dd43a25297481ce6f617816bc630b
wiber <wiber@DESKTOP-29TD6KD>
2024-06-30 19:13:47 -0300

    INICIALIZACION PROYECTO TP 4

---
+++ src/main/java/ar/edu/unju/fi/Tp04AltamiranoLaraMaiaDaiApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp04AltamiranoLaraMaiaDaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp04AltamiranoLaraMaiaDaiApplication.class, args);
+	}
+
+}
